f6cf2454b5499bfba079329035c9e0e9d21d1956
beeva-valentinquintas <valentin.quintas@beeva.com>
2017-05-25 11:46:05 +0200

    Project skeleton

---
+++ README.md
@@ -0,0 +1,27 @@
+## Style detector
+
+#### Up & Running
+
+Set Google Cloud service credentials
+```
+export GOOGLE_APPLICATION_CREDENTIALS=PATH_TO_KEY_FILE
+```
+Install Google Cloud SDK [documentation](https://cloud.google.com/sdk/docs/)
+
+Authenticate to use your local machine
+```
+gcloud auth application-default login
+```
+
+Generate virtual environment
+```
+virtualenv -p python2 virtual_env_folder
+source virtual_env_folder/bin/activate
+cd modernometro
+pip install -r requirements.txt
+```
+
+Run it
+```
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+
+import io
+from google.cloud import vision
+
+
+def main():
+    client = vision.Client()
+
+    # The name of the image file to annotate
+    file_name = os.path.join(
+        os.path.dirname(__file__),
+        'resources/traje.jpg')
+
+    # Loads the image into memory
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
+        image = client.image(
+            content=content)
+
+    # Performs label detection on the image file
+    labels = image.detect_labels()
+
+    print('Labels:')
+    for label in labels:
+        print(label.description)
+
+if __name__ == "__main__":
+    main()
