107d8c19a678daca99f19ac7e79f2f644ce02c6e
enrique.otero <enrique.otero@beeva.com>
2017-05-29 15:07:08 +0200

    Parameterize input file

--- main.py
+++ main.py
@@ -2,15 +2,30 @@ import os
 
 import io
 from google.cloud import vision
+import argparse
+import sys
 
+FILENAME='resources/traje.jpg'
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-filename", help='filename')
+    args = parser.parse_args()
+    if args.filename is None:
+        parser.print_help()
+        sys.exit(-1)
+    return args
 
 def main():
+
+    args = parse_args()
+
     client = vision.Client()
 
     # The name of the image file to annotate
     file_name = os.path.join(
-        os.path.dirname(__file__),
-        'resources/traje.jpg')
+        os.path.dirname(__file__),args.filename
+        )
 
     # Loads the image into memory
     with io.open(file_name, 'rb') as image_file:
