f8fa87fe1bfd73348a002d1ddc31568033519fec
beeva-valentinquintas <valentin.quintas@beeva.com>
2017-05-29 15:16:49 +0200

    removed unsused file reader

--- main.py
+++ main.py
@@ -8,15 +8,12 @@ def main():
     client = vision.Client()
 
     # The name of the image file to annotate
-    file_name = os.path.join(
-        os.path.dirname(__file__),
-        'resources/traje.jpg')
+    file_name = 'resources/traje.jpg'
 
     # Loads the image into memory
     with io.open(file_name, 'rb') as image_file:
         content = image_file.read()
-        image = client.image(
-            content=content)
+        image = client.image(content=content)
 
     # Performs label detection on the image file
     labels = image.detect_labels()
