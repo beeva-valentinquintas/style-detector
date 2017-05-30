d50fba7b89051f17a7276d6abacb2e432ef4a6cb
enrique.otero <enrique.otero@beeva.com>
2017-05-30 12:24:39 +0200

    Transform json according to labels.csv

--- main.py
+++ main.py
@@ -7,9 +7,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+import csv
+
+# Dictionary of jsons. Keys are labels. Values are json {style: x, points: n}
+mydict={}
+for row in csv.reader(open('resources/labels.csv')):
+    mydict[row[0]] = {'style': row[1], 'label': row[0], 'points': row[2]}
 
 @app.route("/labels")
 def get_labels():
+
     client = vision.Client()
     file_name = 'resources/traje.jpg'
 
@@ -21,10 +28,16 @@ def get_labels():
     # Performs label detection on the image file
     labels = image.detect_labels()
 
+    global mydict
     labels_json = {'Labels':[]}
+    styles_json = {'Styles':[]}
     for label in labels:
         labels_json['Labels'].append(label.description)
-    return json.dumps(labels_json)
+        if (label.description) in mydict.keys():
+            style = mydict[label.description]
+            #print style
+            styles_json['Styles'].append(style)
+    return json.dumps(styles_json)
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=True)
