39873b0e1c378df3fe5fd1668c6c79027e05f23c
beeva-valentinquintas <valentin.quintas@beeva.com>
2017-06-01 11:10:26 +0200

    Web to take pictures added to repository

---
+++ web/README.md
@@ -0,0 +1,15 @@
+## Camera shutter
+
+This simple web take a picture each time it's loaded and
+send it to a localhost backend
+
+In order to use it you need to run it in a local server
+ like SimpleHTTPServer.
+Just run:
+
+    cd style-detector/web
+    python -m SimpleHTTPServer
+
+You can access the web page on
+
+    localhost:8000
---
+++ web/index.js
@@ -0,0 +1,66 @@
+
+var videoInput = document.getElementById('inputVideo');
+var canvasInput = document.getElementById('inputCanvas');
+
+$( document ).ready(function() {
+	htracker = new headtrackr.Tracker({
+        ui: false,
+        //debug: canvasInput
+        debug: false
+    });
+    htracker.init(videoInput, canvasInput);
+    htracker.start();
+
+    setTimeout(function(){ getUserFaces(); }, 3000);
+
+});
+
+function getUserFaces(){
+
+	var dataCaptured = canvasInput.toDataURL("image/png").replace("image/png", "");
+
+    $("#imageCaptured").attr("src", dataCaptured );
+
+    console.log( dataCaptured );
+
+    labels = sendImage(dataCaptured);
+
+    console.log(labels);
+}
+
+function convertToCognitiveFormat(dataURL) {
+    var BASE64_MARKER = ';base64,';
+    if (dataURL.indexOf(BASE64_MARKER) == -1) {
+        var parts = dataURL.split(',');
+        var contentType = parts[0].split(':')[1];
+        var raw = decodeURIComponent(parts[1]);
+        return new Blob([raw], { type: contentType });
+    }
+    var parts = dataURL.split(BASE64_MARKER);
+    var contentType = parts[0].split(':')[1];
+    var raw = window.atob(parts[1]);
+    var rawLength = raw.length;
+
+    var uInt8Array = new Uint8Array(rawLength);
+
+    for (var i = 0; i < rawLength; ++i) {
+        uInt8Array[i] = raw.charCodeAt(i);
+    }
+
+    return new Blob([uInt8Array], { type: contentType });
+}
+
+
+function sendImage(imageAsBytes) {
+    var parts = imageAsBytes.split(',');
+    url = "http://127.0.0.1:5000/labels"
+    var oReq = new XMLHttpRequest();
+    oReq.open("POST", url, true);
+    oReq.onload = function (oEvent) {
+      // Uploaded.
+    };
+
+    var blob = new Blob([parts[1]], {type: 'text/plain'});
+
+    oReq.send(blob);
+}
