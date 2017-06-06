1e85b7968c4ff6cde991d3a00e3dd4aee0e3dad6
beeva-valentinquintas <valentin.quintas@beeva.com>
2017-06-06 11:43:22 +0200

    Save image and labels

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import json
+import time
 from flask import request, jsonify
 from flask_cors import cross_origin
 from google.cloud import vision
 from flask import Flask
 import base64
+import io
+from PIL import Image
+
+from os.path import join, dirname
+
 from styles_comparator import StylesComparator
 
 app = Flask(__name__)
@@ -20,7 +27,9 @@ def get_labels():
     logos_objects = image.detect_logos()
     logos = [str(logo.description).lower() for logo in logos_objects]
     labels.extend(logos)
-    
+
+    save_labels_and_image(decoded_image, labels_objects)
+
     style = StylesComparator(request.data)
     twin_response = style.find_twin(labels)
     style.save_new_image_and_labels(labels)
@@ -30,6 +39,17 @@ def get_labels():
         response = jsonify(style.create_output(labels, twin_response))
     response.status_code = 200
     return response
+
+
+def save_labels_and_image(decoded_image, labels_objects):
+    labels_with_scores = [{"label": str(label.description.lower()), "score": label.score} for label in labels_objects]
+    timestamp = str(time.time())
+    filename = join(dirname(__file__), "test_images", timestamp)
+    with open(filename, 'w') as fd:
+        json.dump(labels_with_scores, fd)
+    filename += '_image'
+    image = Image.open(io.BytesIO(decoded_image))
+    image.save(filename, format="JPEG")
 
 
 if __name__ == "__main__":
